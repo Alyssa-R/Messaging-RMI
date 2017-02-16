0be6438b363243cffd3251ac26db3da7ccc7ec28
Ian Hill <ian.hill@students.olin.edu>
2017-02-16 17:45:23 -0500

    Debugging!

--- Client.java
+++ Client.java
@@ -16,7 +16,7 @@ public class Client implements ClientInterface {
   public Client() {}
 
   public Boolean push(String sender, String message) {
-    System.out.println(">>> " + sender + ": " + message);
+    System.out.println(sender + "> " + message);
     return true;
   }
 
@@ -26,31 +26,53 @@ public class Client implements ClientInterface {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String host = (args.length < 1) ? null : args[0];
     try {
-      Registry registry = LocateRegistry.getRegistry(host);
-      ServerInterface serverStub = (ServerInterface) registry.lookup("ServerInterface");
+      Client client = new Client();
+      ClientInterface stub = (ClientInterface) UnicastRemoteObject.exportObject(client, 0);
+      // Bind the remote object's stub in the registry
+      Registry clientRegistry = LocateRegistry.getRegistry();
+      clientRegistry.bind("ClientInterface", stub);
+
+      Registry serverRegistry = LocateRegistry.getRegistry(host);
+      ServerInterface serverStub = (ServerInterface) serverRegistry.lookup("ServerInterface");
       System.out.print("Client ready\nSelect your username: ");
-      String username = br.readLine();
-      while (!serverStub.register(username, InetAddress.getLocalHost().getHostAddress())) {
-        System.out.println("Someone already has that username. Sorry!\nTry another username: ");
+
+      String username = "";
+      int usernameSuccess;
+      do {
         username = br.readLine();
-      }
+        usernameSuccess = serverStub.register(username, InetAddress.getLocalHost().getHostAddress());
+        if (usernameSuccess == 1){
+         System.out.print("Someone already has that username. Sorry!\nTry another username: ");
+        }
+        else if(usernameSuccess == 2){
+          System.out.print("Username invalid if it contains a colon. \nTry another username: ");
+        }
+      } while (usernameSuccess != 0);
       System.out.print("Welcome " + username + "\n" +
         "Use [recipient username]: [message] to send a message.\n" +
-        "Type \\directory to list the users connected to this server.\n");
+        "Type \\directory to list the users connected to this server.\n" +
+        "Type \\quit to exit the chatroom.\n>>> ");
       String cmd = "";
-      while (br.ready()) {
-        cmd += br.readLine();
+      while ((cmd = br.readLine()) != null) {
         if (cmd.equals("\\directory")) {
           String[] directory = serverStub.getDirectory();
           for (int i = 0; i < directory.length; i++) {
             System.out.println(directory[i]);
           }
-        } else {
+        }
+        else if (cmd.equals("\\quit")) {
+          System.out.print("Done!\n");
+          br.close();
+          clientRegistry.unbind("ClientInterface");
+          UnicastRemoteObject.unexportObject(client, true);
+          return;
+        }
+        else {
           Matcher m = validMsg.matcher(cmd);
           if (m.find()) {
-            boolean success = serverStub.send(username, m.group(0), m.group(1));
+            boolean success = serverStub.send(username, m.group(1), m.group(2));
             if (success) {
-              System.out.println("Message sent to " + m.group(0));
+              System.out.println("Message sent to " + m.group(1));
             } else {
               System.out.print("No such user is in the directory.\n");
             }
@@ -58,8 +80,8 @@ public class Client implements ClientInterface {
             System.out.print("Invalid message.\nUse [recipient username]: [message] to send a message.\n");
           }
         }
+        System.out.print(">>> ");
       }
-      br.close();
     } catch (Exception e) {
       System.err.println("Client exception: " + e.toString());
       e.printStackTrace();
--- Server.java
+++ Server.java
@@ -1,4 +1,4 @@
-package MessagingRMI;
+package message;
 
 import java.rmi.NotBoundException;
 import java.rmi.registry.Registry;
@@ -11,20 +11,24 @@ import java.util.Set;
 
 public class Server implements ServerInterface {
   private Hashtable<String, ClientInterface> directory;
-  
-  
+
+
   public Server() {
     directory = new Hashtable<String, ClientInterface>();
   }
-  
+
   /*Returns true and adds username to directory if not already in use
    * If username in use, returns false (fails)
    * establishes each client's connection to server
    */
-  public Boolean register(String name, String ip) {
-    if(directory.contains(name)){
-      //send error?
-      return false;
+  public int register(String name, String ip) {
+    if(directory.containsKey(name)){
+      System.out.println("Error 1: used");
+      return 1;
+    }
+    else if (name.contains(":")){
+      System.out.println("Error 2: colon");
+      return 2;
     }
     else{
       try{
@@ -34,26 +38,27 @@ public class Server implements ServerInterface {
           ClientInterface ClientStub = (ClientInterface) registry.lookup("ClientInterface");
           directory.put(name, ClientStub);
           //confirmation message?
-          return true;
+          return 0;
       }
         catch(RemoteException e){
           System.out.println(e);
-          return false;
+          return 3;
         }
         catch(NotBoundException e){
           System.out.println(e);
-          return false;
+          return 3;
         }
       }
     }
-  
-    
+
+
     /*Returns true if message successfully passed
-     * returns false upon failure 
-     */ 
+     * returns false upon failure
+     */
     public Boolean send(String sender, String recipient, String message) {
       try{
-        if (directory.contains(recipient)){
+        if (directory.containsKey(recipient)){
+
           ClientInterface recipStub = directory.get(recipient);
           recipStub.push(sender,message);
           return true;
@@ -68,28 +73,28 @@ public class Server implements ServerInterface {
         return false;
       }
     }
-    
+
     /*Returns String array of usernames in use in this chat room
-     * 
-     */ 
+     *
+     */
     public String[] getDirectory() {
       Set<String> users = directory.keySet();
       String[] usersArray = new String[users.size()];
       return users.toArray(usersArray);
     }
-    
+
     public static void main(String args[]) {
 
       try {
         Server obj = new Server();
         ServerInterface stub = (ServerInterface) UnicastRemoteObject.exportObject(obj, 0);
-        
+
         // Bind the remote object's stub in the registry
         Registry registry = LocateRegistry.getRegistry();
         registry.bind("ServerInterface", stub);
-        
+
         System.err.println("Server ready");
-        
+
         //get the client interface -- remove these altogether? establishing connection in register username, seems risky
         //get registry
         /*
