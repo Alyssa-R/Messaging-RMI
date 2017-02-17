14b9cef4eb2718aba7cc5d54e26069c7f84271cf
Ian Hill <ian.hill@students.olin.edu>
2017-02-16 21:02:53 -0500

    Add comments to the client

--- Client.java
+++ Client.java
@@ -14,26 +14,36 @@ public class Client implements ClientInterface {
 
   public Client() {}
 
+  /**
+   * Provides a RMI handle to allow the server to send messages to the client.
+   */
   public Boolean push(String sender, String message) {
     System.out.println(sender + "> " + message);
     return true;
   }
 
+  /**
+   * Defines the behavior of the client console and calls RMI methods on the
+   * server.
+   */
   public static void main(String[] args) throws IOException {
-    Pattern validMsg = Pattern.compile("([a-zA-Z0-9]++): (.++)");
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // Read the IP address of the server from the terminal arguments.
     String host = (args.length < 1) ? null : args[0];
+    // Prepare the regex pattern for sending messages.
+    Pattern validMsg = Pattern.compile("([a-zA-Z0-9]++): (.++)");
+    // Set up a buffered reader to read input from the command line.
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     try {
+      // Expose this client to a server through the RMI registry.
       Client client = new Client();
       ClientInterface stub = (ClientInterface) UnicastRemoteObject.exportObject(client, 0);
-      // Bind the remote object's stub in the registry
       Registry clientRegistry = LocateRegistry.getRegistry();
       clientRegistry.bind("ClientInterface", stub);
-
+      // Attempt to connect to the server.
       Registry serverRegistry = LocateRegistry.getRegistry(host);
       ServerInterface serverStub = (ServerInterface) serverRegistry.lookup("ServerInterface");
+      // Initialize the variables for the username request loop.
       System.out.print("Client ready\nSelect your username: ");
-
       String username = "";
       int usernameSuccess;
       do {
@@ -46,18 +56,23 @@ public class Client implements ClientInterface {
           System.out.print("Username invalid if it contains a colon. \nTry another username: ");
         }
       } while (usernameSuccess != 0);
+      // Once the user has successfully chosen a username, we can provide
+      // instructions for using the chatroom.
       System.out.print("Welcome " + username + "\n" +
         "Use [recipient username]: [message] to send a message.\n" +
         "Type \\directory to list the users connected to this server.\n" +
         "Type \\quit to exit the chatroom.\n>>> ");
+      // Initialize the variables
       String cmd = "";
       while ((cmd = br.readLine()) != null) {
+        // Handle a request for the directory from the server.
         if (cmd.equals("\\directory")) {
           String[] directory = serverStub.getDirectory();
           for (int i = 0; i < directory.length; i++) {
             System.out.println(directory[i]);
           }
         }
+        // Exist the application.
         else if (cmd.equals("\\quit")) {
           System.out.print("Done!\n");
           br.close();
@@ -65,6 +80,7 @@ public class Client implements ClientInterface {
           UnicastRemoteObject.unexportObject(client, true);
           return;
         }
+        // Otherwise, try to interpret the input as a message.
         else {
           Matcher m = validMsg.matcher(cmd);
           if (m.find()) {
